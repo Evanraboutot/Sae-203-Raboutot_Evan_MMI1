e3e6251e2f8ba7f9ef20d16571fd110503a62cd2
Evanraboutot <evan.raboutot@edu.univ-fcomte.fr>
2022-06-03 10:16:04 +0200

    Ajout des informations des cartes

---
+++ src/contacts.js
@@ -0,0 +1,26 @@
+// Contenu des cards de profil des artistes
+
+export default [
+	{
+		name: 'The Inspector Cluzo',
+		desc: 'Un duo de génie mettant en avant la polyculture interconnecté à l’environnement',
+		avatar: '/PROG_INSPECTOR_CLUZO.jpg',
+	},
+
+	{
+		name: 'Sims',
+		desc: 'le rappeur nous propose un nouveau voyage dans une société éclatée comme jamais avec l’ambition de raconter une réalité et de faire vivre, le temps d’un concert les joies d une époque',
+		avatar: '/PROG_SIMS-1.jpg',
+	},
+	{
+		name: 'Holy Field',
+		desc: 'HOLY FIELD est à l’avant-garde du renouveau du Post Nu-Metal qui conquiert actuellement le monde.',
+		avatar: '/PROG_Holy-field-2.png',
+	},
+	{
+		name: 'The Jamborines',
+		desc: 'Cette fois-ci, le groupe plonge plus profondément dans les mélodies et harmonies vocales. Cependant, ne vous laissez pas berner par le son rêveur «Beachboy-esque» des Jamborines, leur show sans concession a du punch, nous rappelant par la même occasion qu’ils ne sont pas seulement des rêveurs',
+		avatar: 'PROG_jamborines.jpg',
+	}
+
+]
