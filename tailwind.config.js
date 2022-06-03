671c85b57c6882371a1a67a778ab305838f701c9
Evanraboutot <evan.raboutot@edu.univ-fcomte.fr>
2022-06-03 10:53:07 +0200

    Ajout du dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  purge: {
+    content: [
+      "./index.html",
+      "./src/**/*.{vue,js,ts,jsx,tsx}",
+    ],
+  },
+  darkMode: 'class',
   theme: {
     extend: {},
   },
