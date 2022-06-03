0e173cf9726f662ee6eb92d453c0a97ebb779ab1
Evanraboutot <evan.raboutot@edu.univ-fcomte.fr>
2022-06-03 10:04:14 +0200

    Création du footer

---
+++ src/components/icons/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+	<footer class="text-gray-100">
+		<section class="w-full flex flex-col gap-10 md:flex-row md:justify-between lg:justify-center">
+			<section class="md:w-5/12">
+
+				<ul>
+					<li>
+						<i class="fab fa-whatsapp text-green-400 mr-2"></i>
+						+1242422423
+					</li>
+					<li>
+						<i class="far fa-envelope text-red-400 mr-2"></i>
+						FeelTheRock@gmail.com
+					</li>
+					<li>
+						<i class="fab fa-instagram text-purple-400 mr-2"></i>
+						FeelThe-ROCK
+					</li>
+				</ul>
+			</section>
+		</section>
+		<section class="w-full text-center mt-16">
+			FeelTheRock-Evan Raboutot &copy; 2022
+		</section>
+	</footer>
+</template>
