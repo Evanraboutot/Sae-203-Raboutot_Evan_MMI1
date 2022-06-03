137ddee6bb0c76027831c3e615957d3c9af6c071
Evanraboutot <evan.raboutot@edu.univ-fcomte.fr>
2022-06-03 10:33:03 +0200

    Définition du bouton PDF

---
+++ src/components/About.vue
@@ -0,0 +1,15 @@
+
+//Définition bouton pdf
+<template>
+	<main class="py-10">
+		<h1 class="text-center pb-6 text-3xl md:text-4xl lg:text-5xl font-semibold text-indigo-500">Programmes</h1>
+		<p class="text-gray-600 text-center lg:px-24">Voici ci dessous le programme en format PDF de ce festival </p>
+		<div class="flex items-center justify-center">
+					<a href="https://docs.google.com/document/d/1Pq2xJfF6lUfCXtgL9v69XljEVZSzHw7zLnQ08WOEuSY/edit?usp=sharing" class="duration-300 cursor-pointer active:ring-2 ring-indigo-300 hover:scale-90 text-gray-50 w-8/12 mt-4 lg:mt-8 flex items-center justify-center gap-2 lg:gap-3 bg-gradient-to-r from-indigo-500 to-indigo-700 py-3 px-3 lg:py-4 rounded-lg block">
+				<i class="fa fa-download lg:text-xl"></i>
+				<span class="lg:text-xl">programme du festival</span>
+			</a>
+		</div>
+
+	</main>
+</template>
