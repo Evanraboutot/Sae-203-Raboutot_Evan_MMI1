7dda7dd7658b729a1053f6ad104af29aeaa3c175
Evanraboutot <evan.raboutot@edu.univ-fcomte.fr>
2022-06-03 09:59:14 +0200

    Ajout de la première page du site

---
+++ src/components/icons/Headline.vue
@@ -0,0 +1,24 @@
+<template>
+	<main class="z-10 mt-20 md:py-10 lg:pt-28 md:flex md:flex-row-reverse md:justify-between md:items-center gap-3">
+  
+    <!-- Hero image -->
+		<img src="/vector.png" class="md:w-5/12 block mb-8 lg:mb-0" />
+		
+		<!-- Première page -->
+		<section class="md:w-6/12">
+			<h1 id="heading" class="lg:text-5xl lg:leading-snug text-3xl text-gray-50 font-semibold">
+				Feel The Pop Rock  &#128075;
+			</h1>
+			<p class="lg:text-4xl text-gray-100 font-medium mt-4">Notre festival:</p>
+			<br>
+			<p class="lg:text-2xl text-gray-100 font-medium">
+				Feel the Poprock Festival s’est incrusté dans le paysage des festivals de musique en Nord franche comté.
+				Au fil des années nous avons eu à coeur de l’améliorer, tant au niveau de l’infrastructure que de la programmation.
+				Cali, BB Brunes, Gerald de Palmas, Michael Jones, Phanee de Pool et bien d’autres.
+			</p>
+			
+
+		</section>
+		
+	</main>
+</template>
