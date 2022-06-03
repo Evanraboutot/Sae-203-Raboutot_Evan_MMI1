c7b75301a88d68d63a851cb5f4582cfad81eb0e0
Evanraboutot <evan.raboutot@edu.univ-fcomte.fr>
2022-06-03 10:38:37 +0200

    FAQ contenu JS

---
+++ src/FAQ.js
@@ -0,0 +1,73 @@
+// Contenu de la partie FAQ
+
+export default [
+	{
+		num: 1,
+		question: 'Où puis-je acheter mon pass ?',
+		answer: `
+		Sur www.FeelTheRock.fr, ou bien sur place à l'office de tourisme de Montbéliard. 
+		`
+	},
+	{
+		num: 2,
+		question: 'Finalement je ne pourrais pas venir au festival, puis-je me faire rembourser ?',
+		answer: `
+		Non. Il faut dans ce cas-là essayer de revendre son billet à un ami ou sur des sites prévus à cet effet comme ticketswap.fr.
+		`
+	},
+	{
+		num: 3,
+		question: 'Puis-je acheter mon pass directement le jour du festival ?',
+		answer: `
+		Il y aura des places à vendre sur place les 3 jours de festival si les concerts ne sont pas complets.
+		`
+	},
+	{
+		num: 4,
+		question: 'Quelles sont les conditions générales de vente ?',
+		answer: `
+		IMPORTANT
+		Chaque billet électronique est muni d’un code-barres permettant l’accès au festival à une seule personne. Il est impossible d’être admis à l’entrée plusieurs fois avec le même billet. La reproduction du billet est interdite. En cas de perte ou de vol ou de duplication d’un billet valide, SEULE la première personne à présenter le billet pourra accéder à l’évènement. L’acheteur demeure responsable de l’utilisation faite du billet électronique. Ce billet devra être présenté lors de votre arrivée à l’entrée du site.
+		
+		Les objets suivants sont interdits : appareil enregistreur, appareil photo, caméra, bouteille en verre, canette, alcool, et d’une manière générale tout objet pouvant se révéler dangereux. Les animaux ne sont pas acceptés. Liste non exhaustive.
+		L’évènement ne dispose pas de consigne. Les mesures de sécurité impliquent une fouille, notamment des sacs. Merci de venir avec le strict nécessaire.
+		
+		L’horaire et l’ordre de passage des artistes sont susceptibles d’être modifiés.
+		Programmation sous réserve.
+		
+		
+		CONDITIONS D’UTILISATION
+		Ce billet électronique est soumis aux conditions générales de vente et aux conditions particulières d’utilisation spécifiques que vous avez acceptées avant l’impression de ce titre. Si elles ne sont pas respectées, elles rendront le billet non valable :
+		
+		Conditions d’impression et de validité du billet : s’il est imprimé, le billet doit l’être sur du papier A4 blanc, vierge, recto et verso, sans modification de taille d’impression et au format portrait (vertical) avec une imprimante jet d’encre ou laser. Le billet doit disposer d’une bonne qualité d’impression. Un billet partiellement imprimé, souillé, endommagé ou illisible ne sera pas accepté et considéré de fait comme non valable. En cas de mauvaise qualité d’impression, vous devez imprimer à nouveau votre billet électronique. 
+		Utilisation et validité du billet : Ce billet n’est ni échangeable, ni remboursable. Il est personnel et incessible. 
+		Lors du contrôle à l’entrée du lieu de l’événement, une pièce d’identité en cours de validité avec photo pourra vous être demandée (passeport, permis de conduire, carte d’identité ou carte de séjour). Vous devez conserver ce billet pendant toute la durée de votre présence sur le lieu de l’événement. En cas de perte ou de vol, nous déclinons toute responsabilité sur l’utilisation illicite de ce billet.
+		L’annulation partielle de la programmation artistique n’est pas un motif de remboursement. En cas d’annulation ou de report de concert(s), le remboursement de ce billet sera soumis aux conditions de l’organisateur de l’événement. De manière générale, les manifestations se déroulent sous la seule responsabilité de l’organisateur. En cas d’annulation totale d’une soirée, vous serez informé des modalités de remboursement sur le site internet du festival. En raison de l’évolution des contraintes sanitaires, l’organisation de l’évènement peut être modifiée.
+		
+		
+		AUTRES MENTIONS
+		Ce billet électronique fait office de facture. En achetant un billet pour FeelTheRock, vous acceptez que votre image soit utilisée sur des photos ou vidéos vouées à la promotion de l’événement. 
+		`
+	},
+	{
+		num: 5,
+		question: 'Peut-on se restaurer sur le site du festival ?',
+		answer: `
+		Plusieurs stands de restaurations, divers et variés, sont disponibles sur différents lieux du festival.
+		`
+	},
+	{
+		num: 6,
+		question: 'Y a-t-il un âge minimum autorisé pour venir à FeelTheRock ?',
+		answer: `
+		Non mais nous conseillons aux mineurs de moins de 16 ans d’être accompagnés d’un majeur.
+		`
+	},
+	{
+		num: 7,
+		question: 'Y a-t-il une infirmerie ou un centre de secours ?',
+		answer: `
+		Il y a un poste de secours à l’intérieur de l’enceinte des concerts ainsi que des médecins et infirmiers.
+		`
+	}
+]
